8000ba4292f9bd5cb24d493fad79051e16d6880c
Casper Thygesen <cth@trifork.com>
2024-01-20 16:02:20 +0000

    play with resume and pause

--- src/Cheetah.WebApi/Infrastructure/Installers/KafkaInstaller.cs
+++ src/Cheetah.WebApi/Infrastructure/Installers/KafkaInstaller.cs
@@ -3,6 +3,9 @@ using Cheetah.WebApi.Core.Config;
 using Confluent.Kafka;
 using Cheetah.Kafka.Extensions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Cheetah.Kafka.Configuration;
+using Cheetah.Kafka;
 
 namespace Cheetah.WebApi.Infrastructure.Installers
 {
@@ -12,18 +15,37 @@ namespace Cheetah.WebApi.Infrastructure.Installers
         {
             var kafkaConsumerOptions = configuration.Get<KafkaConsumerConfig>();
             services.AddCheetahKafka(configuration, options =>
-    {
-        options.ConfigureDefaultConsumer(config =>
-        {
-            config.AllowAutoCreateTopics = false;
-            config.GroupId = kafkaConsumerOptions.ConsumerName;
-            config.AutoOffsetReset = AutoOffsetReset.Latest;
-            config.EnableAutoCommit = true;
-        });
-    })
-    // todo: SetErrorHandler && SecurityProtocol
-    .WithConsumer<Ignore, string>()
-    .WithProducer<string, string>();
+            {
+                options.ConfigureDefaultConsumer(config =>
+                {
+                    config.AllowAutoCreateTopics = false;
+                    config.GroupId = kafkaConsumerOptions.ConsumerName;
+                    config.AutoOffsetReset = AutoOffsetReset.Latest;
+                    config.EnableAutoCommit = true;
+                });
+            })
+            // todo: SetErrorHandler && SecurityProtocol
+            .WithConsumer<Ignore, string>()
+            .WithProducer<Null, string>();
+
+            // services.AddSingleton<IProducer<Null, string>>((localProvider) =>
+            // {
+            //     var kafkaConfig = localProvider.GetRequiredService<IOptions<KafkaConfig>>();
+            //     var factory = localProvider.GetRequiredService<KafkaClientFactory>();
+            //     factory.CreateProducerBuilder<Null, string>(kafkaConfig.Value.Url)
+            //         .AddCheetahOAuthentication(localProvider)
+            //         .Build();
+
+            //     return new ProducerBuilder<Null, string>(
+            //                     new ProducerConfig
+            //                     {
+            //                         BootstrapServers = kafkaConfig.Value.Url,
+            //                         SaslMechanism = SaslMechanism.OAuthBearer,
+            //                         SecurityProtocol = SecurityProtocol.SaslPlaintext,
+            //                     })
+            //                     .AddCheetahOAuthentication(localProvider)
+            //                     .Build();
+            // });
         }
     }
 }
--- src/Cheetah.WebApi/Presentation/Controllers/KafkaController.cs
+++ src/Cheetah.WebApi/Presentation/Controllers/KafkaController.cs
@@ -5,6 +5,7 @@ using Cheetah.WebApi.Core.Config;
 using Confluent.Kafka;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Cheetah.WebApi.Presentation.Controllers
@@ -16,13 +17,15 @@ namespace Cheetah.WebApi.Presentation.Controllers
     public class KafkaController : ControllerBase
     {
         private readonly IConsumer<Ignore, string> _kafkaConsumer;
-        private readonly IProducer<string, string> _kafkaProducer;
+        private readonly IProducer<Null, string> _kafkaProducer;
         private readonly IOptions<KafkaProducerConfig> _kafkaProducerConfig;
+        private readonly ILogger<KafkaController> _logger;
 
         public KafkaController(IConsumer<Ignore, string> kafkaConsumer,
-        IProducer<string, string> kafkaProducer, IOptions<KafkaProducerConfig> kafkaProducerConfig)
+        IProducer<Null, string> kafkaProducer, IOptions<KafkaProducerConfig> kafkaProducerConfig , ILogger<KafkaController> logger)
         {
             _kafkaProducerConfig = kafkaProducerConfig;
+            _logger = logger;
             _kafkaConsumer = kafkaConsumer;
             _kafkaProducer = kafkaProducer;
         }
@@ -36,13 +39,24 @@ namespace Cheetah.WebApi.Presentation.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetMessage()
         {
+            _logger.LogDebug("Resuming from {topicPartition}", _kafkaConsumer.Assignment);
+            _kafkaConsumer.Resume(_kafkaConsumer.Assignment);
             var msg = _kafkaConsumer.Consume(TimeSpan.FromMilliseconds(100)); // todo: make configurable
+
             if (msg?.Message == null)
             {
+                _kafkaConsumer.Pause(_kafkaConsumer.Assignment);
+                _logger.LogDebug("Pausing {topicPartition}", _kafkaConsumer.Assignment);
+
                 return NotFound("No messages left!");
             }
             else
             {
+                _kafkaConsumer.Commit(msg); // Commit the offset
+
+                _kafkaConsumer.Pause(_kafkaConsumer.Assignment);
+                _logger.LogDebug("Pausing {topicPartition}", _kafkaConsumer.Assignment);
+
                 return Ok(msg.Message?.Value);
             }
         }
@@ -56,7 +70,7 @@ namespace Cheetah.WebApi.Presentation.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> ProductMessage(string message)
         {
-            var msg = await _kafkaProducer.ProduceAsync(_kafkaProducerConfig.Value.Topic, new Message<string, string> { Value = message });
+            var msg = await _kafkaProducer.ProduceAsync(_kafkaProducerConfig.Value.Topic, new Message<Null, string> { Value = message }); // Note: producing synchronously is slow and should generally be avoided.
             return Ok($"msg sent at offset: {msg.Offset.Value} for topic: {msg.Topic}");
         }
     }
